2f2486326ce58b125cd1e2866c29efa76a756b64
ffeast <ffeast@gmail.com>
2019-05-12 14:50:16 +0300

    + initial

---
+++ README.md
@@ -0,0 +1,41 @@
+# quik-lua-heartbeat
+
+It's just simple `lua` script to track what's going on with a [QUIK](https://arqatech.com/ru/products/quik/terminals/user-applications/quik-workstation/) trading terminal.
+The script is to be run from inside QUIK so that it always knows what's happening.
+
+The metrics file is meant to be polled by an external tool to write its updates
+for subsequent analysis & alerts if something goes wrong
+# Installation
+1. Download this repo to the computer with QUIK to be monitored installed
+2. Adjust the settings in `heartbeat.lua` if the default ones don't match your needs. This step is optional 
+3. Launch the terminal
+4. Run `heartbeat.lua` via `Services -> Lua scripts` as you would run any other `.lua` script
+5. If everything works as expected you'd end up having some data written into `REPO_PATH\metrics\metrics.txt` where `REPO_PATH` is the folder you installed this repo into.
+6. After that configure your monitoring system to pick up the data from this file 
+
+# Metrics format
+Here's an example of a single line written to the metrics file:
+~~~~70:avg_sent:129278
+1557650470:avg_ping_duration:0.093
+1557650470:orders:32
+1557650470:trades:39
+1557650470:account_balance:0
+1557650470:stop_orders:1
+1557650470:futures_client_holding:1
+1557650470:is_connected:1
+1557650470:avg_rect:288
+1557650470:max_ping_duration:0.281
+~~~~
+
+The format is as follows:
+`<timestamp>:<key>:<value>\r\n`
+
+* timestamp - UNIX timestamp indicating when the key/value was _collected_ by the script
+* key - what's tracked
+* value - the value for the respective key
+
+# Configuration
+The settings can be changed in the `heartbeat.lua` itself
+* `METRICS_PATH` - where the metrics file needs to be written to
+* `REWRITE_EVERY_N_RECORDS` - how often the metrics file needs to be truncated to avoid its infinite growth
+* `SLEEP_PERIOD_MSEC` - the delay between metrics collection/writes. It's set to __1 minute__ by default. Feel free to set any meaningful value 
---
+++ heartbeat.lua
@@ -0,0 +1,55 @@
+--[[
+QUIK https://arqatech.com/ru/products/quik/ lua hearbeat script
+When the script is running from inside QUIK it would constantly write metrics with session parameters
+This file can be further picked up by a monitoring tool for further analysis / alerting
+
+Nice README, questions & fixes: https://github.com/ffeast/quik-lua-heartbeat
+Just put this script into C:\QuikPath\lua folder and run it to start writing metrics
+--]]
+
+-- where to save the metrics file. Feel free to set any path you want
+METRICS_PATH = getScriptPath() .. '\\metrics\\metrics.txt'
+-- delay between metrics writes. Set a lower value to write metrics more often
+SLEEP_PERIOD_MSEC = 60 * 1000
+-- the metrics file will be rewritten after this number of write_metrics call
+-- to prevent infinite metrics file growth
+REWRITE_EVERY_N_RECORDS = 1000
+
+function write_metrics(metrics, counter)
+    local mode
+    if counter % REWRITE_EVERY_N_RECORDS == 0 then
+        mode = 'w'
+    else
+        mode = 'a'
+    end
+
+    local file = io.open(METRICS_PATH, mode)
+    local ts = os.time(os.date('!*t'))
+    for key, value in pairs(metrics) do
+        file:write(ts .. ':' .. key .. ':' .. value .. '\n')
+    end
+    file:flush()
+    io.close(file)
+end
+
+function main()
+    local counter = 0
+    -- we don't catch http://luaq.ru/OnStop.html to survive QUIK restarts
+    while true do
+        counter = counter + 1
+        write_metrics({
+            is_connected = isConnected(),
+            avg_ping_duration = getInfoParam('AVGPINGDURATION'),
+            avg_sent = getInfoParam('AVGSENT'),
+            avg_rect = getInfoParam('AVGRECV'),
+            max_ping_duration = getInfoParam('MAXPINGDURATION'),
+            orders = getInfoParam('orders'),
+            futures_client_holding = getNumberOf('futures_client_holding'),
+            trades = getNumberOf('trades'),
+            orders = getNumberOf('orders'),
+            stop_orders = getNumberOf('stop_orders'),
+            account_balance = getNumberOf('account_balance')
+        }, counter)
+        sleep(SLEEP_PERIOD_MSEC)
+    end
+end
